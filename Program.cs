214dfdc873c9258cf52b3248ef62b467c5ac9369
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-11-13 17:52:15 -0800

    Add New Messages to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GA_GitHub_Tutorial
+﻿using System.Threading.Channels;
+
+namespace GA_GitHub_Tutorial
 {
     internal class Program
     {
@@ -11,6 +13,11 @@
             Console.WriteLine("GitHub works with Git to save those repositories online. Backing up my code and letting me work with others.\n");
 
             Console.WriteLine("I will start by ADDING GIT TO MY PROJECT\n");
+
+            Console.WriteLine("I have added Git to my project\n");
+            Console.WriteLine("I can now start committing my changes\n");
+            Console.WriteLine("Committing is how we tell Git to remember what changes I've made.\n");
+            Console.WriteLine("I can easily do this by Adding a Message and clicking commit in the Git Changes window\n");
 
         } // Main
 
